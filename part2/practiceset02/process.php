0fef971de955739bf25be01c67a446d079c30944
usernamewin <winonalvarez@gmail.com>
2025-02-18 21:16:23 +0800

    tokenazation, wordFrequencyCalculator, and sortingOption functions has been added to the world frequency calculator

--- part2/practiceset02/process.php
+++ part2/practiceset02/process.php
@@ -25,4 +25,55 @@ function readInputText(): string
     return ($_SERVER["REQUEST_METHOD"] == "POST") ? $_POST['text'] : '';
 }
 
+/**
+ * Tokenize the given text into an array of words
+ * 
+ * Remove punctuations and convert text into lowercase
+ * 
+ * The PREG_SPLIT_NO_EMPTY flag is used to prevent empty elements from being included in the array when splitting a string
+ * Ensures that consecutive delimiters do not result in empty strings in the output
+ * 
+ * @param string $text The input text to be tokenized
+ * @return array|false An array of words from the input text, or false if the input text is empty or cannot be tokenized
+ */
+
+function tokenazation(string $text): array|false {
+    return preg_split('/\W+/', strtolower($text), -1, PREG_SPLIT_NO_EMPTY);
+}
+
+/**
+ * Calculates the frequency of each word in the given array of words, excluding stop words
+ *
+ * @param array $words An array of words to be counted
+ * @param array $stopWords An array of stop words not excluded from the count
+ * @return array An array where the keys are the words and the values are their frequencies
+ */
+
+function wordFrequencyCalculator(array $words, array $stopWords): array {
+    $wordFrequency = array();
+
+    foreach ($words as $word) 
+    {
+        if (!in_array($word, $stopWords)) {
+            $wordFrequency[$word] = isset($wordFrequency[$word]) ? $wordFrequency[$word] + 1 : 1;
+        }
+    }
+
+    return $wordFrequency;
+}
+
+/**
+ * Sorts the given associative array of word frequencies in the specified order
+ *
+ * @param array $wordFrequency An array where the keys are words and the values are their frequencies
+ * @param string $sortingOrder The order in which to sort the word frequencies, either ascending ordescending
+ * @return array A sorted array where the keys are words and the values are their frequencies
+ */
+
+function sortingOption(array $wordCounts, string $sortingOrder): array
+{
+    $sortingOrder == 'asc' ? asort($wordCounts) : arsort($wordCounts);
+    return $wordCounts;
+}
+
 ?>
